194451dd5c2488759d3943ab664022146fa2f313
hyejoo <joo95102@naver.com>
2024-06-09 21:24:53 +0900

    차트 옵션

--- src/routes/Coin/Chart/index.tsx
+++ src/routes/Coin/Chart/index.tsx
@@ -2,6 +2,7 @@ import { useCoinChartData } from '@src/routes/Coin/Chart/useCoinChartData';
 import { useOutletContext } from 'react-router-dom';
 import ApexChart from 'react-apexcharts';
 import Loader from '@src/components/Loader';
+import { theme } from '@src/style/theme';
 
 const Chart = () => {
   const { coinId } = useOutletContext<{ coinId: string }>();
@@ -9,20 +10,48 @@ const Chart = () => {
 
   return (
     <>
-      <h3>CHART</h3>
       {chartDataLoading ? (
-        <Loader />
+        <p>Loading</p>
       ) : (
         <ApexChart
           type="line"
-          series={[{ name: 'aa', data: chartData.map((price) => price.close) }]}
+          series={[
+            {
+              name: 'close',
+              data: chartData.map((price) => price.close),
+            },
+          ]}
           options={{
             chart: {
               height: 500,
               width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: 'trnasparent',
+            },
+            grid: {
+              show: false,
             },
             theme: {
               mode: 'light',
+            },
+            stroke: {
+              curve: 'smooth',
+              width: 2,
+            },
+            colors: [theme.bgColor],
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              labels: { show: false },
+              axisTicks: {
+                show: false,
+              },
+              axisBorder: {
+                show: false,
+              },
             },
           }}
         />
